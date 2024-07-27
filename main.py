95e0b7d6dd1f43ac311de5528938ea34616e9e3e
Shrinkhal <97280075+Shrinkhal01@users.noreply.github.com>
2024-07-27 22:39:36 +0530

    Update main.py
    added some comments

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import requests
 from PIL import Image
 from io import BytesIO  # helps image reading using byte
 import os
-os.makedirs('originals',exist_ok=True)
-os.makedirs('masked', exist_ok=True)
-image_path = 'nameLogo.png'
-image = Image.open(image_path)
+os.makedirs('originals',exist_ok=True)#makes à folder named originals
+os.makedirs('masked', exist_ok=True)#makes a folder named masked
+image_path = 'nameLogo.png'#the name of the file whose background is to be removed
+image = Image.open(image_path)#opens the image in the path specified
 imgar = Image.open(image_path)
 imgar = remove(imgar)
 masked_path = 'masked/' + os.path.basename(image_path)
